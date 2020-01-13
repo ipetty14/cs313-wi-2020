9d988a5d2510e91df856705fdd1759020796dd1a
Ian Petty <kanopiellc@gmail.com>
2020-01-13 16:46:40 -0500

    fix cart

--- resources/views/layouts/shoppingLayout.blade.php
+++ resources/views/layouts/shoppingLayout.blade.php
@@ -55,7 +55,7 @@
                         @endif
                         <div class="row">
                             <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
-                                <a href="{{ url('cart') }}" class="btn btn-primary btn-block">View all</a>
+                                <a href="{{ url('week03/cart') }}" class="btn btn-primary btn-block">View all</a>
                             </div>
                         </div>
                     </div>
